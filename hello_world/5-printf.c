9137d8d4e58b015dcfc62532d30a73009e27c55c
santiago2221212 <santihouseman@gmail.com>
2025-02-19 17:44:19 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - principal funtion
+*Return: return 0 if the program runs without problems
+*/
+int main(void)
+{
+	/*printf: will print the next message*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
